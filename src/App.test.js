a38b57101e99f8be5067f7e63ee3c717ffeba66a
Bashorun Olajide John <bashorunolajide@gmail.com>
2022-10-26 11:16:03 +0100

    remove test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,2 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  const element = render(<App />);
-  const linkElement = element.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
